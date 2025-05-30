7915e630455da358c9ce4a5744ca5d632ab3ff24
sori <euns2674@gmail.com>
2025-05-30 15:39:50 +0900

    v1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,38 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import BookForm from './BookForm';
 
-function App() {
-  
-  fetch('http://localhost:8080/hello')
-    .then(response => response.text())
-    .then(data => {
-      console.log(data); // JSON 응답 처리
-  });
+// 기본 페이지 컴포넌트
+function HomePage() {
+  const navigate = useNavigate();
+
+  const handleNewClick = () => {
+    navigate('/form');
+  };
 
   return (
-    <div className="App">
-      
+    <div style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',  //세로
+          alignItems: 'center'       //가로
+    }}>
+      <h1>BOOK LIST</h1>
+      <button onClick={handleNewClick} style={{ padding: '10px 20px', fontSize: '16px' }}> 등록 </button>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />        {/*기본*/}
+        <Route path="/form" element={<BookForm />} />    {/*BookFrame*/}
+      </Routes>
+    </Router>
   );
 }
 
