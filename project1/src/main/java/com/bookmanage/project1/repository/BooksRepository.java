886431db460fdb5ed78145dd0ee837ecb0b0ff57
leejunyup <eeee0102@naver.com>
2025-05-30 15:14:12 +0900

    repository까지

--- project1/src/main/java/com/bookmanage/project1/repository/BooksRepository.java
+++ project1/src/main/java/com/bookmanage/project1/repository/BooksRepository.java
@@ -20,4 +20,7 @@ public interface BooksRepository extends JpaRepository<Books, Long> {
 
     // 제목에 특정 키워드가 포함된 책 조회 (부분 검색)
     List<Books> findByTitleContaining(String keyword);
+
+    // 장르로 책 검색
+    List<Books> findByGenreContaining(String genre);
 }
