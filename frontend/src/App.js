229772a73b364fc94f337919dff6fa076083e186
hyeongmin <nsby8882@naver.com>
2025-05-30 11:35:56 +0900

    front back 연결 확인

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  fetch('http://localhost:8080/hello')
+    .then(response => response.text())
+    .then(data => {
+      console.log(data); // JSON 응답 처리
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
